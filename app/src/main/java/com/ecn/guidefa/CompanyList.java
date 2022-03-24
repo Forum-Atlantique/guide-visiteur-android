763fb25db45c1ee46c9ed1d8846417dc28e877ce
hydrielax <alexidelage@gmail.com>
2022-03-24 08:49:10 +0100

    New companylist activty

---
+++ app/src/main/java/com/ecn/guidefa/CompanyList.java
@@ -0,0 +1,14 @@
+package com.ecn.guidefa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompanyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_list);
+    }
+}
